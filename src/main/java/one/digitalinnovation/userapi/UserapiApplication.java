60cd9d30d799f1b2d365e4d3d5ec22c3d212abc5
Ryam Sousa <ryam.sousa123@gmail.com>
2021-06-22 13:13:51 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/userapi/UserapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserapiApplication.class, args);
+	}
+
+}
