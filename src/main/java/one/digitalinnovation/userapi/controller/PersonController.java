5c3b124b1e5abc8ad285e0f3c08bb97108b8e1a9
Ryam Sousa <ryam.sousa123@gmail.com>
2021-06-26 22:57:37 -0300

    Developed delete method

--- src/main/java/one/digitalinnovation/userapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/userapi/controller/PersonController.java
@@ -34,7 +34,13 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
-        return  personService.findById(id);
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteByIid(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 
 
